25ac7e015eeb2c65432ec90aaca74a820bac2aad
Franz Noel Tanglao <franz@tanglao.org>
2018-08-25 13:41:43 -0700

    feat: Add members, profile, reports, and sidebar components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MembersComponent } from './members/members.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ReportsComponent } from './reports/reports.component';
+
+const appRoutes: Routes = [
+  { path: 'members', component: MembersComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'reports', component: ReportsComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SidebarComponent,
+    MembersComponent,
+    ProfileComponent,
+    ReportsComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
